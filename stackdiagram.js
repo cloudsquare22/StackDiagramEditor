9bc30412b838ad6059ca9e9f1274bcb7c3ea6dc8
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-06 17:18:11 +0900

    Del unuse log.

--- stackdiagram.js
+++ stackdiagram.js
@@ -449,9 +449,6 @@ function drawPlusIcon(element) {
 }
 
 function resizeCanvas() {
-    console.log('resizeCanvas()')
-    console.log($('#canvas_width').val())
-    console.log($('#canvas_height').val())
     $('#canvas').attr("width", $('#canvas_width').val())
     $('#canvas').attr("height", $('#canvas_height').val())
     $('#canvas').drawLayers();
